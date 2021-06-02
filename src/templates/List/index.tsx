4280e5e690d8fedeaa58a0fc4be6f2082948ac75
Kayne <kayne@ciand.com>
2021-06-02 01:47:06 -0300

    Add postList template

---
+++ src/templates/List/index.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import Layout from '../../components/Layout';
+import PostSection from '../../components/PostSection';
+
+const PostList = props => {
+  const postList = props.data.allMarkdownRemark.edges
+
+  const { currentPage, numPages } = props.pageContext;
+  const isFirst = Boolean(currentPage === 1)
+  const isLast = Boolean(currentPage === numPages)
+  const prevPage = currentPage - 1 === 1 ? '/' : `/page/${currentPage-1}`
+  const nextPage = `page/${currentPage+1}` 
+
+  return (
+    <Layout 
+      isIndex={isFirst}
+      isFirst={isFirst}
+      isLast={isLast}
+      currentPage={currentPage}
+      numPages={numPages}
+      prevPage={prevPage}
+      nextPage={nextPage}
+    >
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { title, category, date, },
+            timeToRead,
+            excerpt,
+            fields: { slug },
+          },
+        }) => (
+          <PostSection
+            id="post"
+            title={title}
+            slug={slug}
+            category={category}
+            date={date}
+            timeToRead={timeToRead}
+            description={excerpt}
+          />
+        )
+      )}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query PostList($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            category
+            date(locale: "pt-br", formatString: "MMMM DD, YYYY")
+            title
+          }
+          timeToRead
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+export default PostList;
